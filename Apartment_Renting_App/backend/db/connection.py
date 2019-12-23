d5d36b9697ed38e2ba589e2421501d78a1e379ec
Adeel Bhatti <39279381+adeel182@users.noreply.github.com>
2019-12-22 18:55:58 -0800

    Update connection.py

--- Apartment_Renting_App/backend/db/connection.py
+++ Apartment_Renting_App/backend/db/connection.py
@@ -6,20 +6,20 @@
 # Instructor: Dragutin Petkovic    #
 ####################################
 import mysql.connector
-conn = mysql.connector.connect(host = "csc648-dbinstance.covsgblvixwf.us-east-2.rds.amazonaws.com",
-                            database = 'CSC648',
-                            user = 'root',
-                            password = 'root1234',
-                            auth_plugin='mysql_native_password')
-cursor = conn.cursor()
-
-#
-# conn = mysql.connector.connect(host = "localhost",
+# conn = mysql.connector.connect(host = "csc648-dbinstance.covsgblvixwf.us-east-2.rds.amazonaws.com",
 #                             database = 'CSC648',
 #                             user = 'root',
-#                             password = 'Derectorjin1',
+#                             password = 'root1234',
 #                             auth_plugin='mysql_native_password')
 # cursor = conn.cursor()
+
+#
+conn = mysql.connector.connect(host = "localhost",
+                            database = 'CSC648',
+                            user = 'root',
+                            password = 'Derectorjin1',
+                            auth_plugin='mysql_native_password')
+cursor = conn.cursor()
 
 
 
